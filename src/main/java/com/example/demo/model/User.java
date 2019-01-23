7ffbe4f4434b84334b7acb19c444be2f5766bc6e
why <15809859070@163.com>
2019-01-23 11:09:12 +0800

    123why

--- src/main/java/com/example/demo/model/User.java
+++ src/main/java/com/example/demo/model/User.java
@@ -3,9 +3,11 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 
 @Entity
+@Table(name = "user")
 public class User {
 
     @Id
